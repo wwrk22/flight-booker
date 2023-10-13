db88984ca631bb632d098f9a739128e7914c4236
Won Rhim <wwrk22@gmail.com>
2023-10-13 15:34:06 -0400

    seed some sample flights

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,8 @@
-# Airports
+# Flight has an Airport foreign key, so order matters here.
+Flight.destroy_all
 Airport.destroy_all
+
+# -- Airports --
 
 # Clear the record of unique values returned because unique values ARE limited.
 Faker::Name.unique.clear
@@ -11,5 +14,28 @@ end
 p "Created #{Airport.count} airports"
 
 
-# Flights
+# -- Flights --
 
+# Time in seconds
+thirty_minutes = 1_800
+fifteen_hours = 54_000
+duration_range = (thirty_minutes..fifteen_hours)
+
+# All airport ids
+airport_ids = Airport.pluck :id
+
+100.times do
+  # Pick two random airports
+  departure_airport_id = airport_ids.delete(airport_ids.sample)
+  arrival_airport_id = airport_ids.sample
+
+  Flight.create!({ duration: Faker::Number.within(range: duration_range),
+                   departure_time: Faker::Time.forward(days: 300),
+                   departure_airport_id: departure_airport_id,
+                   arrival_airport_id: arrival_airport_id })
+
+  # Make sure to reset to include all ids for the next flight
+  airport_ids << departure_airport_id
+end
+
+p "Created #{Flight.count} flights"
