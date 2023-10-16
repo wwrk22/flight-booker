9d09628b269c7e9f7e86dc6e74985c41c6d548ac
Won Rhim <wwrk22@gmail.com>
2023-10-16 10:20:36 -0400

    add flight departure time and num passengers to search form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,10 @@
 class FlightsController < ApplicationController
   def index
     @flights = Flight.all
+    @flight_options = @flights.collect do |flight|
+      [flight.departure_time, flight.id]
+    end
+
+    @num_passengers_options = (1..4).map { |n| [n, n] }
   end
 end
