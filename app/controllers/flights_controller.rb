fc9ec64eb96facd8ed2b91ab159adf3aaa298d4d
Won Rhim <wwrk22@gmail.com>
2023-10-16 10:27:11 -0400

    add departure and arrival airport code dropdowns for flight search form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,7 +1,11 @@
 class FlightsController < ApplicationController
   def index
+    @airports_options = Airport.all.map do |airport|
+      [airport.code, airport.id]
+    end
+
     @flights = Flight.all
-    @flight_options = @flights.collect do |flight|
+    @flights_options = @flights.collect do |flight|
       [flight.departure_time, flight.id]
     end
 
